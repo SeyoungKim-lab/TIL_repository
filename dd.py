1b7562fa7d1c57142c04e820dc4e0cc31f266c8f
tpdudss <tpdudss@naver.com>
2026-01-22 17:57:27 +0900

    aa

--- dd.py
+++ dd.py
@@ -1,68 +1,64 @@
-# def recur_example(number):
-#     '''
-#         함수(2) 실행
-#             number에 2 할당
-#             if 2 == 1 조건문 만족하지 않음
-#             else문 2 + 함수(2-1) 
-#                 결과를 알기위해서는 함수(2-1)의 실행 결과가 필요
+list_of_book = [
+    '장화홍련전',
+    '가락국 신화',
+    '온달 설화',
+    '금오신화',
+    '이생규장전',
+    '만복자서포기',
+    '수성지',
+    '백호집',
+    '원생몽유록',
+    '홍길동전',
+    '장생전',
+    '도문대작',
+    '옥루몽',
+    '옥련몽',
+]
 
-#                 함수(2-1) 실행
-#                     number에 1 할당
-#                     if 1 == 1 조건문 만족하므로 1 반환
-            
-#             else문의 2 + 함수(2-1)중, 함수(2-1)의 실행결과가 1임을 알게되었음 
-#             2 + 1 반환
-#         결과 : 3  
-#     '''
-#     if number == 1:
-#         return 1
-#     else:
-#         return number + recur_example(number - 1)
-# result_1 = recur_example(5)
-# print(result_1) # 5 + 4 + 3 + 2 + 1 = 15
+rental_list = [
+    '장생전',
+    '원생몽유록',
+    '이생규장전',
+    '장화홍련전',
+    '수성지',
+    '백호집',
+    '난중일기',
+    '홍길동전',
+    '만복자서포기',
+]
+for rental_book in rental_list:
+    for list_book in list_of_book:
+        if list_book == rental_book:
+            break 
+    break
 
-# 거듭 제곱 재귀 함수
-# base = 밑, exponent = 지수
-# base의 exponent승 == 2의 3승
-def power(base, exponent):
-    '''
-        함수(2, 3) 실행
-            base에 2 할당, exponent에 3할당
-            지수가 0이 된 경우, 1을 반환 | 2의 0승은 1
+print('모든 도서가 대여 가능한 상태입니다.')
+'''
+list_book==rental_book 이면 첫번째for문으로 이동
+즉, 장생전 == 장화홍련전 아니니까 이때부터 바로 print를 하는상황.
 
-            아닌경우, 지수가 0이 될 때까지 [exponent - 1] 을 다시 지수에 할당하여 함수 호출
-                2 * 함수(2, 3-1)
+for rental_book in rental_list:
+    for list_book in list_of_book:
+        if list_book == rental_book:
+            break 
+이상태면 장생전==장화홍련전 아니니까 다시 안쪽 for문 돌다가
+장생전==장생전 되면 다시 바깥 for문으로 가야함. ㅇㅋ
+그러다가 잘가다가 난중일기일때,
+끝까지 난중일기가 없어. 그럼 탈출해야해.
+난중일기==옥련몽까지 했는데 아니야. 그러면 아예 탈출해야돼
 
-            모든 상황을 반복하는 과정
-            2 * (2 * (2 * 1))  
-            결과 : 8
-    '''
-    if  exponent == 0:
-        return 1
-    else: 
-        
-        return base*power(base,exponent-1)
-result_2 = power(2, 3)
-print(result_2) # 2 * 2 * 2 * 1 = 8
 
-# # 모든 자릿수 더하기 함수
-# def sum_of_digits(number):
-#     '''
-#         함수(321) 실행
-#         number가 10 미만인 경우, number 반환
 
-#         아닌경우, number가 10 미만이 될 때까지, number를 10으로 나눈 몫을 다시 number에 할당하여 함수 호출
-#             number를 10으로 나누 나머지 + 함수(number를 10으로 나눈 몫)
-#             1 + (321 // 10)
+'''
 
-#         모든 상황을 반복하는 과정
-#         1 + 2 + 3
-#         결과 : 6
-#     '''
-#     if :
-#         return 
-#     else:
-#         return 
-# result_3 = sum_of_digits(321)
-# print(result_3) # 1 + 2 + 3 = 6
 
+'''
+rental_book에 '장생전'이 할당됨
+이때 장생전이 list_of_book 에 있는지 확인해야함
+이걸 어떻게확인하지
+if rental_book = list_of_book[0~끝] 맞는지 확인하고싶음
+그러면 if문 안에서 또 반복문을 해야할듯
+for list_book in list_of_book
+if list_book = rental_book
+break 
+'''

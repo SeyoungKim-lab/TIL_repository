383dad3eaeb1e172e728d51f42e25db1407672ad
KSY <tpdudss@naver.com>
2026-01-27 15:43:56 +0900

    a

--- dd.py
+++ dd.py
@@ -1,12 +1,23 @@
-
 # 아래 함수를 수정하시오.
-def capitalize_words(m):
-    list_m = m.split()
-    for i in list_m:
-        i[0].upper()
-    a = "".join(list_m)
-    return a
+def find_min_max(lst):
+    # 일단 맨 앞에 있는 값을 최소값,최대값이라 생각
+    min_v = lst[0]
+    max_v = lst[0]
+
+    # 최소값과 최대값을 구하는 과정
+    for num in lst:
+        # 지금 꺼내온 num이 min_v 보다 작다
+        if num < min_v :
+            min_v = num
+        # 지금 꺼내온 num 이 max_v 보다 크다
+        if num > max_v:
+            # 발견한 더 큰 값을 최대값으로 변경
+            max_v = num
 
 
-result = capitalize_words("hello, world!")
-print(result)
+
+    return min_v, max_v
+
+
+result = find_min_max([3,1,7,2,5])
+print(result) #(1,7)
--- ff.py
+++ ff.py
@@ -1,12 +1,28 @@
-# 아래 함수를 수정하시오.
-def even_elements(L):
-    for i in L:
-        if i%2 == 0:
-            evennum_index = L.index(i)
-            L.pop(evennum_index)
-    L.pop()
+# 딕셔너리를 입력받아 value와 key를 뒤집은 결과를 반환하는 함수 `dict_invert()`를 작성하기
 
 
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-result = even_elements(my_list)
-print(result)
+# 1. [] 표기법을 사용한 방법
+def dict_invert(input_dict):
+    lst_reversed = {}
+    for key,value in input_dict.items():
+        
+
+
+# 2. get 메서드를 사용한 방법
+def dict_invert(input_dict):
+    pass
+
+
+# 3. defaultdict를 사용한 방법
+from collections import defaultdict
+
+
+def dict_invert(input_dict):
+    pass
+
+
+print(dict_invert({1: 10, 2: 20, 3: 30}))  # {10: [1], 20: [2], 30: [3]}
+print(
+    dict_invert({1: 10, 2: 20, 3: 30, 4: 30})
+)  # {10: [1], 20: [2], 30: [3, 4]}
+print(dict_invert({1: True, 2: True, 3: True}))  # {True: [1, 2, 3]}

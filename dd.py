1723cbbf79e6aaafd00c8b508c4682aedef79e40
KSY <tpdudss@naver.com>
2026-01-29 17:58:38 +0900

    a

--- dd.py
+++ dd.py
@@ -1,23 +1,68 @@
-# 아래 함수를 수정하시오.
-def find_min_max(lst):
-    # 일단 맨 앞에 있는 값을 최소값,최대값이라 생각
-    min_v = lst[0]
-    max_v = lst[0]
+class UserInfo:
+    def __init__(self):
+        self.user_data = {}
 
-    # 최소값과 최대값을 구하는 과정
-    for num in lst:
-        # 지금 꺼내온 num이 min_v 보다 작다
-        if num < min_v :
-            min_v = num
-        # 지금 꺼내온 num 이 max_v 보다 크다
-        if num > max_v:
-            # 발견한 더 큰 값을 최대값으로 변경
-            max_v = num
+    def get_user_info(self):
+        """
+        사용자로부터 이름과 나이를 입력받습니다.
+        - 이름이 없거나 공백이면 None을 반환합니다.
+        - 나이가 숫자가 아니거나 입력되지 않으면 ValueError를 처리하고 False를 반환합니다.
+        - 올바르게 입력되면 사용자 정보를 저장하고 True를 반환합니다.
+        """        
+        # TODO: 아래 코드를 문제 요구사항에 맞게 완성하세요.
+        self.dic = {}
+        self.name = input('이름을 입력하세요: ')
+        self.name_strip = self.name.strip()
+        
+        if self.name_strip == "":
+            return None
+        
+            
+            
+        
+        try:
+            self.age = int(input('나이를 입력하세요: '))
+            self.dic['이름'] = self.name_strip
+            self.dic['나이'] = self.age
+            return True
+        
+        except:
+            print("나이는 숫자로 입력해야 합니다.")
+            return False
+        
+        
+
+    
+
+
+        
+        
+
+    def display_user_info(self):
+        """
+        저장된 사용자 정보를 출력합니다.
+        - 정보가 없으면 "사용자 정보가 입력되지 않았습니다."를 출력합니다.
+        """
+        # TODO: 아래 코드를 문제 요구사항에 맞게 완성하세요.
+        if result is True:
+            print(f"사용자 정보:")
+            print(f"이름: {self.dic['이름']}")
+            print(f"나이: {self.dic['나이']}")
+        elif result is None:
+            print("사용자 정보가 입력되지 않았습니다.")
+
+
+# 아래 코드는 수정하지 마세요.
+user = UserInfo()
+result = user.get_user_info()
 
 
 
-    return min_v, max_v
 
-
-result = find_min_max([3,1,7,2,5])
-print(result) #(1,7)
+if result is True:
+    user.display_user_info()
+elif result is None:
+    # 이름이 입력되지 않은 경우, display_user_info()가 적절한 메시지를 출력해야 합니다.
+    user.display_user_info()
+# 나이가 잘못 입력된 경우 (result is False), get_user_info()에서 이미 메시지를 출력했으므로
+# 추가적인 동작이 필요 없습니다.
--- test1.py
+++ test1.py
@@ -1,3 +1,1 @@
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-k = range(len(my_list))
-print(k)
+print(3 and 'b')

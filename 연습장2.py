25ee8d7c0baa5b89fd0b59ac3a0f9558376e6f18
SeyoungKim <tpdudss@naver.com>
2026-03-07 14:25:04 +0900

    pycharm

--- 연습장2.py
+++ 연습장2.py
@@ -1,99 +1,64 @@
-import copy
-import sys
 from collections import deque
+import sys
+sys.stdin = open('input.txt', 'r')
+ 
+ 
+N = int(input())
 
-sys.stdin = open("aa.txt", "r")
-#N : 지도의 새로길이
-#M : 지도의 가로길이
-N, M = map(int, input().split())
+village = [list(map(int,input())) for _ in range(N)]
 
-JIDO = [list(map(int, input().split())) for _ in range(N)]
+# 문제이해.
+# 마을에 있는 단지들에 번호를 붙이고,
+# 단지의 수를 출력하고,
+# 그 단지 내에 있는 집의 개수를 단지별로 오름차순 정렬하여라.
 
-# 1. 문제상황
-# 빈칸(0)에 벽(1)을 3개세우는데,
-# 어디다 세워야 바이러스(2)가 최소로 퍼지는가?
-# 그때 빈칸의 갯수.
+# 알고리즘/자료구조
+# cnt_estate cnt_home 변수두개만든다. sub_list 리스트하나만든다.
+# for문으로 행우선순회하며, 1의 위치를 찾는다.
+# 찾으면 cnt_estate 에 1을 더한다.
 
-# 2. 알고리즘/자료구조
-# N,M은 3에서8사이 이므로, N*M 해봤자 최대 64.
-# 벽 3개를 모든 곳에 다 세워본다해도 복잡도가 그리 크지 않을것으로예상.
+# 그 위치를 시작점으로 하여 bfs탐색을 한다.
+# bfs탐색은 함수로 따로 만들어보자.
+# 들린 곳은 visited처리 대신, cnt_estate 으로 바꾸자. 
+# 들릴때마다 cnt_home에 1을더한다.
 
-# 벽3개를 가능한 모든 곳에 세워보고, 2에서의 BFS탐색을 채택.
-# 2의위치를 찾아서 list에 넣어둔다.
-# 0의 위치도 찾아서 lst에 넣어둔다.
-# 조합: 재귀를 통해 3개의벽을 랜덤하게 다 세워본다.
-#     : 세우는 경우들에 대해(종료return하기직전에) BFS탐색을 진행한다. 
-#     : BFS를 하기전 지도의 deepcopy본을 만들어 거기서 진행한다.
-#     : 카피본에서 탐색하고 바이러스를 다채워주고,
-#     : 0의 개수를 센다. 그리고 최댓값 갱신
-virus_list = []
-def find_virus():
-    for i in range(N):
-        for j in range(M):
-            if JIDO[i][j] == 2:
-                virus_list.append((i,j))
-find_virus()
-count_2 = len(virus_list)
+# 한 단지에대해 bfs가 끝나면, sub_list에 cnt_home을 append한다.
 
-empty_list = []
-def find_empty():
-    for i in range(N):
-        for j in range(M):
-            if JIDO[i][j] == 0:
-                empty_list.append((i,j))
-find_empty()
-count_0 = len(empty_list)
+# 그리고 마지막 cnt_estate 값에서 1을 빼면 그것이 단지의 수이고,
+# sub_list 를 sort 하여 오름차순정렬한다.
+# cnt_estate-1 을 출력하고, sub_list를 for문순회하며 출력한다.
 
+cnt_estate = 0
+sub_list = []
 
-def count_safe(temp):
-    counts = 0
-    for i in range(N):
-        for j in range(M):
-            if temp[i][j] == 0:
-                counts += 1
-    return counts
-
-max_v = 0   # 0의 최대개수
-def make_wall(start,depth):
-    global max_v
-    if depth == 3:
-        temp = copy.deepcopy(JIDO)
-        bfs(temp)
-        cnt = count_safe(temp)
-        if max_v < cnt:
-            max_v = cnt
-        return
-    
-    for i in range(start, count_0):
-        x, y = empty_list[i]
-        JIDO[x][y] = 1  # 벽세우기
-        make_wall(i+1, depth+1)
-        JIDO[x][y] = 0  # 벽지우기
-
-def bfs(temp):
-    #델타탐색
+def bfs(i,j):
     di = [-1,1,0,0]
     dj = [0,0,-1,1]
-    #큐
-    q = deque(virus_list)
-
-        
+    q = deque([(i,j)])
+    cnt_home = 1
+    village[i][j] = 0
     while q:
-        vi, vj = q.popleft()   #현위치
-        
+        ni,nj = q.popleft()
         for d in range(4):
-            wi = vi + di[d]
-            wj = vj + dj[d]
-            if 0<=wi<N and 0<=wj<M and temp[wi][wj] == 0:
+            wi = ni + di[d]
+            wj = nj + dj[d]
+            if 0<=wi<N and 0<=wj<N and village[wi][wj]==1:
                 q.append((wi,wj))
-                temp[wi][wj] = 2
+                village[wi][wj] = 0
+                cnt_home += 1
+    return cnt_home
 
-make_wall(0,0)
-print(max_v)
+for i in range(N):
+    for j in range(N):
+        if village[i][j] == 1:
+            cnt_estate+=1
+            sub_list.append(bfs(i,j))
 
+sub_list.sort()
 
-        
-        
-
+print(cnt_estate)
+for i in sub_list:
+    print(i)
+            
 
 

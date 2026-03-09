6c35811331b5dac77e8233a0882cd2fa775890ca
Jaehyeon <masdf13@naver.com>
2026-03-09 17:48:33 +0900

    2026_03_09

--- 연습장2.py
+++ 연습장2.py
@@ -1,64 +1,25 @@
-from collections import deque
-import sys
-sys.stdin = open('input.txt', 'r')
- 
- 
-N = int(input())
+# 주사위 3개를 던져서 합이 10이하인 케이스의 수
 
-village = [list(map(int,input())) for _ in range(N)]
+# 상태공간트리
+# 주사위3개 => depth 3
+# branch 수: 1~6숫자 -> 6
 
-# 문제이해.
-# 마을에 있는 단지들에 번호를 붙이고,
-# 단지의 수를 출력하고,
-# 그 단지 내에 있는 집의 개수를 단지별로 오름차순 정렬하여라.
+result = 0
 
-# 알고리즘/자료구조
-# cnt_estate cnt_home 변수두개만든다. sub_list 리스트하나만든다.
-# for문으로 행우선순회하며, 1의 위치를 찾는다.
-# 찾으면 cnt_estate 에 1을 더한다.
+def recur(cnt, total):
+    global result
+    
+    if total > 10:
+        return
+    
+    if cnt == 3:
+        # 경로의 합이 10 이하라면
 
-# 그 위치를 시작점으로 하여 bfs탐색을 한다.
-# bfs탐색은 함수로 따로 만들어보자.
-# 들린 곳은 visited처리 대신, cnt_estate 으로 바꾸자. 
-# 들릴때마다 cnt_home에 1을더한다.
-
-# 한 단지에대해 bfs가 끝나면, sub_list에 cnt_home을 append한다.
-
-# 그리고 마지막 cnt_estate 값에서 1을 빼면 그것이 단지의 수이고,
-# sub_list 를 sort 하여 오름차순정렬한다.
-# cnt_estate-1 을 출력하고, sub_list를 for문순회하며 출력한다.
-
-cnt_estate = 0
-sub_list = []
-
-def bfs(i,j):
-    di = [-1,1,0,0]
-    dj = [0,0,-1,1]
-    q = deque([(i,j)])
-    cnt_home = 1
-    village[i][j] = 0
-    while q:
-        ni,nj = q.popleft()
-        for d in range(4):
-            wi = ni + di[d]
-            wj = nj + dj[d]
-            if 0<=wi<N and 0<=wj<N and village[wi][wj]==1:
-                q.append((wi,wj))
-                village[wi][wj] = 0
-                cnt_home += 1
-    return cnt_home
-
-for i in range(N):
-    for j in range(N):
-        if village[i][j] == 1:
-            cnt_estate+=1
-            sub_list.append(bfs(i,j))
-
-sub_list.sort()
-
-print(cnt_estate)
-for i in sub_list:
-    print(i)
-            
-
-
+        result += 1
+        return
+    
+    for num in range(1,7):
+        recur(cnt + 1, total + num)
+        
+recur(0, 0)
+print(result)
--- 연습장4.py
+++ 연습장4.py
@@ -1,52 +1,18 @@
+path = []
+N = 7
+used = [0]*N
 
-T =int(input())
-
-for tc in range(1,1+T):
-    # N: square의 세로길이
-    # M: square의 가로길이
-    N, M = map(int, input().split())
-    square = [input() for _ in range(N)]
-    code = ""
-    code_table = {
-    "0001101": "0",
-    "0011001": "1",
-    "0010011": "2",
-    "0111101": "3",
-    "0100011": "4",
-    "0110001": "5",
-    "0101111": "6",
-    "0111011": "7",
-    "0110111": "8",
-    "0001011": "9",
-    }
-    def find_code():
-        global code
-        odd_sum = 0
-        even_sum = 0
-        answer = 0
-        for i in range(N):
-            for j in range(M-1,50, -1):
-                if square[i][j] == "1":
-                    # 코드 의심 부분
-                    line = square[i][j - 55:j + 1]
-                    
-                    for k in range(0,56,7):    # k는 7개코드의 첫숫자
-                        code += code_table[line[k:k+7]]
-                    # 코드 검증 (code의 홀수자리합*3 + 짝수자리합 = 10의배수)
-                    for w in range(4):
-                        even_sum += int(code[w*2+1])
-                        odd_sum += int(code[w*2])
-                    # 10의배수면
-                    if (odd_sum*3 + even_sum)%10 == 0:
-                        for a in code:
-                            answer += int(a)
-                        return answer
-                    # 10의배수가 아니면
-                    else:
-                        return answer
-                        
+def recur2(cnt):
+    if cnt == 3:
+        print(*path)
+        return
     
-    print(f"#{tc} {find_code()}")
-                    
 
-    
+    for i in range(1,7):
+        
+        # used[i] = 1
+        path.append(i)
+        recur2(cnt+1)
+        path.pop()
+        
+recur2(0)

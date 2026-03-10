6fc42ba7bad02d7ed115e30891715ffc86a903d9
Jaehyeon <masdf13@naver.com>
2026-03-10 17:55:05 +0900

    dd

--- 연습장2.py
+++ 연습장2.py
@@ -1,42 +1,6 @@
-import sys
-sys.stdin=open("input.txt", "r")
-
-T = int(input())
-
-for tc in range(1,1+T):
-    # 0부터 N-1 까지의 수
-    N = int(input())
-    golf_area = [list(map(int, input().split())) for _ in range(N)]
-
-    # 1은 첫자리고정, 나머지 수는 수열만들기 + 마지막에 1로 돌아오기까지.
-    # branch는 기본적으로 N인데 used배열을 써서 앞에쓴건 못쓰게.
-    # total에 golf_area[이전숫자][지금숫자] 를 더해 다음depth에 넘겨준다.
-    # depth==0일때는 1고정이므로, depth==1부터 시작.
-
-    used = [0]*N
-    # 0은 이미 사용
-    used[0] = 1
-    min_v = 100*N*N
-    def recur(depth, now_room , total):
-        global min_v
-        # 1. 종료조건
-        if 0 not in used:
-            total = total + golf_area[now_room][0]
-            if min_v > total:
-                min_v = total
-            return
-        # 2. 가지치기
-        if min_v < total:
-            return
-
-        # 3. 재귀호출
-        for next_room in range(N):
-            if used[next_room]:
-                continue
-            used[next_room] = 1
-            e_now_to_next = golf_area[now_room][next_room]
-            recur(depth+1, next_room, total + e_now_to_next)
-            used[next_room] = 0
-
-    recur(0, 0, 0)
-    print(f"#{tc} {min_v}")
+def subset(i, selected, cnt):
+    #1. 종료조건
+    if cnt == R:
+        
+    
+    subset(i+1, selected + [arr[i]])
--- 연습장3.py
+++ 연습장3.py
@@ -1,24 +1,29 @@
 T = int(input())
 
-for tc in range(1,1+T):
-    N = int(input())
-    matrix = [list(map(int,input().split())) for _ in range(N)]
+for tc in range(1, T+1):
     
-    di = [1,0]
-    dj = [0,1]
+    # 1. 무게 중에 가장 큰놈을 담을 수 있는지 적재용량 큰거부터 체크
+    # 2. 담을 수 있다면 담은무게를 어떤 변수에 더해주고 그 적재용량은 0으로 만들어버리기.
     
-    def recur(wi,wj, total):
-        # 1. 종료조건
-        if (wi,wj) == (N-1,N-1):
-            print(total)
-            return
-        # 2. 재귀호출
-        # ↓한번, →한번 총 2번호출
-        for d in range(2):
-            wi = wi + di[d]
-            wj = wj + dj[d]
-            if 0<=wi<N and 0<=wj<N:
-                recur(wi,wj, total + matrix[wi][wj])
-            
+    # N: 컨테이너수
+    # M: 트럭수
+    N, M = map(int, input().split())
+    # W_lst : 컨테이너 무게리스트
+    # t_lst : 트럭 적재용량 리스트
+    W_lst = list(map(int,input().split()))
+    t_lst = list(map(int,input().split()))
     
-    recur(0,0,matrix[0][0])
+    W_lst.sort(reverse=True)
+    t_lst.sort(reverse=True)
+    
+    total_w = 0
+    
+    for i in range(N):
+        for j in range(M):
+            if W_lst[i] <= t_lst[j]:  # 담을 수 있으면
+                total_w += W_lst[i]   # 담기
+                t_lst[j] = 0          # 담은 트럭은 못쓰니까 0으로 만들기
+                break   # 다음 i로 넘어가야함
+    
+    
+    print(f"#{tc} {total_w}")

00b4a164a3f2c392d5889fd7f7323caea228761c
SeyoungKim <tpdudss@naver.com>
2026-03-31 22:30:18 +0900

    d

--- 연습장4.py
+++ 연습장4.py
@@ -4,7 +4,15 @@ sys.stdin = open("input.txt", "r")
 T = int(input())
 
 for tc in range(1,1+T):
-    N = int(input())
-    chu_list = list(map(int, input().split()))
+    candy = list(map(int, input().split()))
+    answer = -1
+    if candy[1] >=2 and candy[2] >= 3:
+        answer = 0
+        if candy[1] > candy[2] - 1:
+            answer += candy[1] - (candy[2] - 1)
+            candy[1] = candy[2] - 1
+        if candy[0] > candy[1] - 1:
+            answer += candy[0] - (candy[1] - 1)
+    print(f"#{tc} {answer}")
     
     

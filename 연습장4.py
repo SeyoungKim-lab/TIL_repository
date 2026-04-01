01e43b4516dfb501248df60f5d2081c122256cec
SeyoungKim <tpdudss@naver.com>
2026-04-01 22:28:02 +0900

    d

--- 연습장4.py
+++ 연습장4.py
@@ -12,39 +12,54 @@ for tc in range(1,1+T):
     # gamepan: 게임판
     gamepan = [list(map(int,input().split())) for _ in range(H)]
 
-    start_cnt = 0
+    remain_cnt = 0
     for i in range(H):
         for j in range(W):
             if gamepan[i][j] != 0:
-                start_cnt += 1
-    # 카피게임판 형성: 얘를 가지고 놀다가 하나의 path가 끝나면 초기화시킬 예정
-    copy_gamepan = copy.deepcopy(gamepan)
+                remain_cnt += 1
+
+
+    # 정리하는 함수
+    def organize(copy_gamepan):
+        global now_remain
+        for j in range(W):              # j는 오른쪽방향으로
+            idx = H-1
+            for i in range(H-1,-1,-1):  # H-1번부터 0번 인덱스까지 역순회
+                if copy_gamepan[i][j] != 0:
+                    copy_gamepan[idx][j], copy_gamepan[i][j] = copy_gamepan[i][j], copy_gamepan[idx][j]
+                    idx -= 1
+        
+        return
 
     # 델타탐색
     di = [-1,1,0,0]
     dj = [0,0,-1,1]
     # 해당 열에 떨어뜨린 후에 정리까지 해야함.
-    def BFS(w):
+    def BFS(w,copy_gamepan):
+        global now_remain
         # 열순회로 첫위치찾기
         for i in range(H):
             if copy_gamepan[i][w] != 0:
                 si, sj = i, w
                 break
-        q = deque([(si,sj)])
-
+        else:
+            return
+        q = deque([(si,sj,copy_gamepan[si][sj])])
+        copy_gamepan[si][sj] = 0
+        now_remain -= 1
         while q:
-            vi,vj = q.popleft()
-            v = copy_gamepan[vi][vj]
-            copy_gamepan[vi][vj] = 0
+            vi,vj,p = q.popleft()
             for d in range(4):
-                for j in range(v):
+                for j in range(p):
                     wi = vi + di[d]*j
                     wj = vj + dj[d]*j
                     if 0<= wi < H and 0<= wj < W and copy_gamepan[wi][wj] != 0:
-                        q.append((wi,wj))
+                        q.append((wi,wj,copy_gamepan[wi][wj]))
+                        copy_gamepan[wi][wj] = 0
+                        now_remain -= 1
         # 다 터뜨렸으면 정리
-
-
+        organize(copy_gamepan)
+        return
     
 
 
@@ -52,23 +67,25 @@ for tc in range(1,1+T):
     min_v = float("inf")
     # W개(0~W-1) 중에 중복순열로 N개를 나열하는 수열
     def permutation(idx):
+        global min_v, now_remain
+        # 가지치기
+        if min_v == 0:
+            return
         # 종료조건
         if idx == N:
-            remain_cnt = 0  # 남은 벽돌수를 저장할리스트
-            
+            now_remain = remain_cnt  # 남은 벽돌수를 저장할리스트
+            copy_gamepan = copy.deepcopy(gamepan)
             # path에 들어있는 순서대로 BFS를 진행
             for w in path:
-                BFS(w)
+                BFS(w,copy_gamepan)
             # BFS는 다 터진후의 게임판copy_gamepan 을 반납
             # 다 터지고 남은 벽돌수 체크
-            for i in range(H):
-                for j in range(W):
-                    if copy_gamepan[i][j] != 0:
-                        remain_cnt += 1
-            min_v = min(min_v, remain_cnt)
-
-            # 다음 path를 위해 카피게임판을 초기화
-            copy_gamepan = copy.deepcopy(gamepan)
+            # for i in range(H):
+            #     for j in range(W):
+            #         if copy_gamepan[i][j] != 0:
+            #             remain_cnt += 1
+            min_v = min(min_v, now_remain)
+            
             return
         
         # 재귀호출
@@ -78,12 +95,9 @@ for tc in range(1,1+T):
             path.pop()
 
     path = []
-    # permutation(0)
-    BFS(2)
-    BFS(2)
-    max_pang = start_cnt - min_v
-    for i in range(H):
-        print(f" {copy_gamepan[i]}")
+    permutation(0)
+
+    print(f"#{tc} {min_v}")
     
     
 

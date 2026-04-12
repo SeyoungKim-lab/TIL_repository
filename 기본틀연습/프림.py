d7b46c543a526429d72f57d4745bcf5a902d2488
SeyoungKim <tpdudss@naver.com>
2026-04-12 22:15:30 +0900

    8hour_study

--- 기본틀연습/프림.py
+++ 기본틀연습/프림.py
@@ -8,54 +8,47 @@ from heapq import heappush, heappop
 # - 갈 수 있는 노드들 중 가중치가 가장 작은 노드부터 간다
 # --> 작은 노드를 먼저 꺼내기 위해 우선순위큐(heapq)를 활용한다
 def prim(start_node):
-    pq = [(0,start_node)] # (가중치, 노드) 형태로 시작점을 담은상태로 큐생성
-    MST = [0] * V          # visited 와 동일하다 - MST 생성
-    min_weight = 0           # 최소 비용 변수 생성
+         # (가중치, 노드) 형태로 시작점을 담은상태로 큐생성
+                  # visited 와 동일하다 - MST 생성
+                   # 최소 비용 변수 생성
     # while문 시작
-    while pq:
+    
         # 힙팝을 해서 현가중치,현노드
-        weight, node = heappop(pq)
+        
         # 이미 방문한 노드라면 continue
-        if MST[node]:
-            continue
-
+        
             
         
         # 현재 노드에서 해야할 액션 => MST포함체크와 누적합추가
-        MST[node] = 1    # queue 에서 pop 될 때 MST 에 포함되는 게 확정!
-        min_weight += weight    # 누적합 추가
+            # queue 에서 pop 될 때 MST 에 포함되는 게 확정!
+        
+            # 누적합 추가
         
 
 
         # 연결된 노드를 탐색(탐색에서 할 액션은 큐에 넣는 것 한가지뿐)
-        for next_weight, next_node in graph[node]:        
+                
             # 이미 방문했으면 continue
-            if MST[next_node]:
-                continue
+            
                 
             # 탐색노드들을 푸시
-            heappush(pq, (next_weight, next_node))
             
     # 최소비용을 리턴하기
-    return min_weight
+    
 
 
 
 # V,E 입력받기
-V, E = map(int, input().split())
+
 # 인접 리스트
-graph = [[] for _ in range(V)]
+
 # E줄 입력받아 인접리스트 완성하기(무향)
-for _ in range(E):
-    start, end, weight = map(int, input().split())
-    graph[start].append((weight,end))
-    graph[end].append((weight, start))
+
         
     
     
 
 # 출발 정점과 함께 시작
-result = prim(4)
 # 출발 정점을 바꾸어도, 최소비용은 똑같다
 # 단, 그래프가 다르게 나올수는 있다.
 
--- 연습장2.py
+++ 연습장2.py
@@ -1,53 +1,84 @@
 import sys
-sys.stdin = open("dijkstra_input.txt", "r")
+sys.stdin = open("input.txt", "r")
 
+T = int(input())
 
-from heapq import heappop, heappush
+# 델타탐색
+di = [-1,1,0,0]
+dj = [0,0,-1,1]
 
-
-def dijkstra(start_node):
-    pq = [(0, start_node)]  # (누적거리, 노드번호)
-    dists = [INF] * V       # 각 정점까지의 최단거리를 저장할 리스트
-    dists[start_node] = 0   # 시작노드 최단거리는 0
-
-    while pq:
-        dist, node = heappop(pq)
-
-        # 이미 더 작은 값으로 온 적이 있으면 버린다
-        #  - (3,c)  (4,c) 예시
-        if dists[node] < dist:      # 어쩌다보니 선택됐는데 더 크면 버린다
+def DFS(now_i, now_j, oppertunity, now_length):
+    global maximum_length
+    # 매번 최대길이 갱신
+    maximum_length = max(maximum_length, now_length)
+    # 탐색
+    for d in range(4):
+        next_i = now_i + di[d]
+        next_j = now_j + dj[d]
+        # 범위 벗어나면 컨티뉴
+        if next_i < 0 or next_i > N-1 or next_j < 0 or next_j > N-1:
             continue
-
-        for next_dist, next_node in graph[node]:
-            # 다음 노드로 가기 위한 누적 거리
-            # 누적 거리 = 현재까지의 거리 + 다음 거리
-            new_dist = dist + next_dist
-
-            # 이미 작거나 같은 가중치로 온 적이 있다면 continue
-            if dists[next_node] <= new_dist:    # 다음꺼를 고르는데 더 크면 버린다.
+        # 방문한적있으면 컨티뉴
+        if visited[next_i][next_j]:
+            continue
+        # 나보다 크거나 같으면
+        if matrix[next_i][next_j] >= matrix[now_i][now_j]:
+            # 기회가 없다면 컨티뉴
+            if oppertunity == False:
                 continue
+            # 근데 기회가 남아있다면?
+            elif oppertunity == True:
+                # 깎았을 때 갈 수 있는지 본다.
+                cutted_height = matrix[next_i][next_j] - K  # 다음위치를 깎아봤을때의 높이
+                # 깎았을 때 갈 수 있다면
+                if cutted_height < matrix[now_i][now_j]:
+                    # 임시저장
+                    temp = matrix[next_i][next_j]
+                    # 깎고(최대로 깎지말고, 현재보다 1만 작게 깎는다.=>그래야 최장길이보장)
+                    matrix[next_i][next_j] = matrix[now_i][now_j] - 1
+                    # 이동한다.
+                    visited[next_i][next_j] = 1     # 이동하기 전 방문체크
+                    DFS(next_i, next_j, False, now_length+1)
+                    visited[next_i][next_j] = 0     # 되돌리기
+                    matrix[next_i][next_j] = temp   # 깎은거 되돌리기
+                    
+                # 깎았는데 갈 수 없다면 컨티뉴
+                else:
+                    continue
+        # 나보다 작으면
+        else:
+            # 그냥 다음 방향으로 이동한다.
+            visited[next_i][next_j] = 1     # 이동하기 전 방문체크
+            DFS(next_i, next_j, oppertunity, now_length+1)
+            visited[next_i][next_j] = 0     # 되돌리기
 
-            # 누적거리, 새로운 노드를 pq에 저장 + dists 에 갱신
-            dists[next_node] = new_dist
-            heappush(pq, (new_dist, next_node))
+for tc in range(1, 1+T):
+    # N: 부지의 크기
+    # K: 최대 깎을 수 있는 높이
+    N, K = map(int, input().split())
+    # matrix: 부지
+    matrix = [list(map(int, input().split())) for _ in range(N)]
+    # visited
+    visited = [[0]*N for _ in range(N)]
+    # 최장등산로길이
+    maximum_length = 0
 
-    return dists
+    max_v = 0
+    # 시작위치 찾기1. max값 찾기
+    for i in range(N):
+        for j in range(N):
+            max_v = max(max_v, matrix[i][j])
+    # 시작위치 찾기2. max값 있는 위치 찾기
+    for i in range(N):
+        for j in range(N):
+            if matrix[i][j] == max_v:
+                # si,sj가 시작위치
+                si, sj = i, j
+                # DFS 진행
+                visited[si][sj] = 1
+                DFS(si,sj,True,1)
+                visited[si][sj] = 0
 
-
-
-INF = int(21e8)  # 무한대를 가정 (문제의 최대)
-                 # 손으로 다 더했을 때 최대값이 몇일까?
-
-V, E = map(int, input().split())
-start_node = 0  # 시작점
-graph = [[] for _ in range(V)]  # 인접 리스트로 구현
-                                # [선택과제] 인접 행렬로 구현
-
-for _ in range(E):
-    start, end, weight = map(int, input().split())
-    graph[start].append((weight, end))  # [주의] 단방향
-
-# 출발지로부터 모든 최단거리
-result = dijkstra(0)
-print(result)
-
+    # 진행하고나면, maximum_length 가 갱신되어있을것임.
+    print(f"#{tc} {maximum_length}")
+    

92e86d28cd157d4a858e24f54a47634bf1a07bef
kimseyoung <tpdudss@naver.com>
2026-05-18 22:05:46 +0900

    d

--- 파이썬연습/연습장1.py
+++ 파이썬연습/연습장1.py
@@ -1,18 +1,17 @@
 import sys
 sys.stdin = open('input.txt', 'r')
+
 from heapq import heappush, heappop
 
-T= int(input())
-
+T = int(input())
 
 def prim(start_node):
     # pq 생성
-    pq = [(0, start_node)]
+    pq = [(0,start_node)]
     # MST 생성
     MST = [0] * (V+1)
     # 최소가중치
     min_weight = 0
-
     # while문 시작
     while pq:
         # 힙팝하며 현위치
@@ -20,7 +19,7 @@ def prim(start_node):
         # 팝하자마자 MST확인
         if MST[now_node]:
             continue
-        # 현위치에서 MST채우고, 최소합 갱신
+        # 현위치에서 할행동 두가지
         MST[now_node] = 1
         min_weight += now_weight
         # 탐색
@@ -32,12 +31,14 @@ def prim(start_node):
             heappush(pq, (next_weight, next_node))
     return min_weight
 
-for tc in range(1, 1+T):
-    # 0에서 V까지
+
+for tc in range(1,1+T):
+    # V: 0에서 V까지
+    # E: 간선의 수
     V, E = map(int, input().split())
     # 인접리스트
     graph = [[] for _ in range(V+1)]
-    # 인접리스트에 입력받기
+    # 인접리스트 채워넣기
     for _ in range(E):
         start, end, weight = map(int, input().split())
         graph[start].append((weight, end))
